dc40ae3fb52b1ae8e93b0ebf6c0eec4d3d536f3d
kPherox <kphrx@kpherox.dev>
2025-06-18 20:49:08 +0900

    Initial commit

---
+++ src/client/kotlin/TemplateModDataGenerator.kt
@@ -0,0 +1,9 @@
+package dev.kpherox.template_mod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object TemplateModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
