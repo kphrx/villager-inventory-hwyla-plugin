68d5bcd5c689f06e53d19a0f782c953cd114d729
kPherox <kphrx@kpherox.dev>
2025-06-30 05:22:43 +0900

    feat: implement wthit server data

--- src/client/kotlin/wthit/VillagerInventoryProvider.kt
+++ src/client/kotlin/wthit/VillagerInventoryProvider.kt
@@ -1,6 +1,15 @@
 package dev.kpherox.vihp.client.wthit
 
+import dev.kpherox.vihp.wthit.VillagerInventoryProvider as VillagerInventoryDataProvider
+
+import io.netty.buffer.Unpooled
+
 import net.minecraft.world.entity.npc.Villager
+import net.minecraft.world.level.Level
+import net.minecraft.nbt.ByteArrayTag
+import net.minecraft.nbt.Tag
+import net.minecraft.world.item.ItemStack
+import net.minecraft.network.RegistryFriendlyByteBuf
 
 import mcp.mobius.waila.api.IEntityAccessor
 import mcp.mobius.waila.api.IEntityComponentProvider
@@ -9,10 +18,22 @@ import mcp.mobius.waila.api.ITooltip
 
 object VillagerInventoryProvider: IEntityComponentProvider {
 	override fun appendBody(tooltip: ITooltip, accessor: IEntityAccessor, config: IPluginConfig) {
-		val inventory = (accessor.getEntity() as Villager).getInventory().getItems()
+		val tag = accessor.getData().raw().get(VillagerInventoryDataProvider.INVENTORY_KEY)
+		if (tag == null) {
+			return
+		}
+		val inventory = decode(accessor.getWorld(), tag)
 
 		for (itemStack in inventory) {
 			tooltip.addLine(itemStack.getHoverName());
 		}
+	}
+
+	fun decode(level: Level, tag: Tag): List<ItemStack> {
+		val buffer = RegistryFriendlyByteBuf(Unpooled.buffer(), level.registryAccess())
+		buffer.writeBytes((tag as ByteArrayTag).getAsByteArray())
+		val items = ItemStack.OPTIONAL_LIST_STREAM_CODEC.decode(buffer)
+		buffer.clear()
+		return items
 	}
 }
---
+++ src/main/kotlin/wthit/VillagerInventoryPlugin.kt
@@ -0,0 +1,19 @@
+package dev.kpherox.vihp.wthit
+
+import net.minecraft.world.entity.npc.Villager
+
+import org.slf4j.LoggerFactory
+import mcp.mobius.waila.api.ICommonRegistrar
+import mcp.mobius.waila.api.IWailaCommonPlugin
+
+object VillagerInventoryPlugin : IWailaCommonPlugin {
+	private val logger = LoggerFactory.getLogger("villager-inventory-hywla-plugin")
+
+	override fun register(registrar : ICommonRegistrar) {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+		registrar.entityData(VillagerInventoryProvider, Villager::class.java)
+	}
+}
