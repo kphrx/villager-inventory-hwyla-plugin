0ac78f39952a7752058ee0d93d4950eae7e007ea
kPherox <kphrx@kpherox.dev>
2025-07-03 03:12:50 +0900

    refactor: use accessor class function when used minecraft api only

--- src/client/kotlin/wthit/VillagerInventoryProvider.kt
+++ src/client/kotlin/wthit/VillagerInventoryProvider.kt
@@ -1,15 +1,6 @@
 package dev.kpherox.vihp.client.wthit
 
-import dev.kpherox.vihp.wthit.VillagerInventoryProvider as VillagerInventoryDataProvider
-
-import io.netty.buffer.Unpooled
-
-import net.minecraft.world.entity.npc.Villager
-import net.minecraft.world.level.Level
-import net.minecraft.nbt.ByteArrayTag
-import net.minecraft.nbt.Tag
-import net.minecraft.world.item.ItemStack
-import net.minecraft.network.RegistryFriendlyByteBuf
+import dev.kpherox.vihp.VillagerInventoryAccessor
 
 import mcp.mobius.waila.api.IEntityAccessor
 import mcp.mobius.waila.api.IEntityComponentProvider
@@ -19,20 +10,8 @@ import mcp.mobius.waila.api.component.ItemListComponent
 
 object VillagerInventoryProvider: IEntityComponentProvider {
 	override fun appendBody(tooltip: ITooltip, accessor: IEntityAccessor, config: IPluginConfig) {
-		val tag = accessor.getData().raw().get(VillagerInventoryDataProvider.INVENTORY_KEY)
-		if (tag == null) {
-			return
-		}
-		val inventory = decode(accessor.getWorld(), tag)
+		val inventory = VillagerInventoryAccessor.getWithDecode(accessor.getData().raw(), accessor.getWorld())
 
 		tooltip.addLine(ItemListComponent(inventory))
-	}
-
-	fun decode(level: Level, tag: Tag): List<ItemStack> {
-		val buffer = RegistryFriendlyByteBuf(Unpooled.buffer(), level.registryAccess())
-		buffer.writeBytes((tag as ByteArrayTag).getAsByteArray())
-		val items = ItemStack.OPTIONAL_LIST_STREAM_CODEC.decode(buffer)
-		buffer.clear()
-		return items
 	}
 }
--- src/main/kotlin/wthit/VillagerInventoryDataProvider.kt
+++ src/main/kotlin/wthit/VillagerInventoryDataProvider.kt
@@ -1,37 +1,16 @@
 package dev.kpherox.vihp.wthit
 
-import net.minecraft.world.entity.npc.Villager
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.Level
-import net.minecraft.nbt.Tag
-import net.minecraft.nbt.ByteArrayTag
-import net.minecraft.network.RegistryFriendlyByteBuf
+import dev.kpherox.vihp.VillagerInventoryAccessor
 
-import io.netty.buffer.Unpooled
+import net.minecraft.world.entity.npc.Villager
 
 import mcp.mobius.waila.api.IServerAccessor
 import mcp.mobius.waila.api.IDataProvider
 import mcp.mobius.waila.api.IPluginConfig
 import mcp.mobius.waila.api.IDataWriter
-import mcp.mobius.waila.api.data.ItemData
 
 object VillagerInventoryProvider: IDataProvider<Villager> {
-	val INVENTORY_KEY = "vihp:inventory"
-
 	override fun appendData(data: IDataWriter, accessor: IServerAccessor<Villager>, config: IPluginConfig) {
-		val inventory = getInventory(accessor.getTarget())
-
-		data.raw().put(VillagerInventoryProvider.INVENTORY_KEY, encode(accessor.getWorld(), inventory))
-	}
-
-	private fun getInventory(villager: Villager) = villager.getInventory().getItems().filterNot { it.`is`(Items.AIR) }
-
-	fun encode(level: Level, items: List<ItemStack>): Tag {
-		val buffer = RegistryFriendlyByteBuf(Unpooled.buffer(), level.registryAccess())
-		ItemStack.OPTIONAL_LIST_STREAM_CODEC.encode(buffer, items)
-		val tag = ByteArrayTag(buffer.array())
-		buffer.clear()
-		return tag
+		VillagerInventoryAccessor.putWithEncode(data.raw(), accessor.getWorld(), accessor.getTarget())
 	}
 }
